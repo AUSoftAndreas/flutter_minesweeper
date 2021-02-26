5ae2c22c3ff985a80c5924573aaeeaa8472e2f0d
Andreas Hoefer <hoefer74@gmail.com>
2021-02-26 01:46:54 +0100

    update

--- lib/notifiers/field_notifier.dart
+++ lib/notifiers/field_notifier.dart
@@ -1,40 +1,16 @@
-import 'dart:developer';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:minesweeper/models/block.dart';
 import 'package:minesweeper/models/field.dart';
 
+/// The FieldNotifier is the connection between GUI and state. All state
+/// changes go through FieldNotifier. It extends StateNotifier which handles
+/// all GUI updates. Part of Riverpod.
 class FieldNotifier extends StateNotifier<Field> {
+  /// Constructor (from Riverpod tutorial)
   FieldNotifier() : super(Field());
 
-  void create({required numRows, required numCols, required mines}) {
+  /// Creates a new playing field ... just by instantiating a new state
+  /// with a new playing field
+  void create({required int numRows, required int numCols, required int mines}) {
     state = Field.withArguments(numRows: numRows, numCols: numCols, mines: mines);
-  }
-
-  void handleClick(Block block) {
-    if (state.gameLost) {
-      //return; // TODO: Wieder rein nehmen
-    }
-    if (state.flagMode) {
-      block.flagged = !block.flagged;
-      state = state.copyWith();
-      return;
-    }
-    if (block.open) return;
-    block.reveal(state.map);
-    if (block.mine) {
-      state.map.forEach(
-        (pos, block) {
-          if (block.mine) block.open = true;
-        },
-      );
-      state = state.copyWith(gameLost: true);
-      return;
-    }
-    state = state.copyWith();
-  }
-
-  void toggleFlagMode() {
-    state = state.copyWith(flagMode: !state.flagMode);
   }
 }
