eeee3ca5774279e1ad48420e685551721a50b57c
Andreas Hoefer <hoefer74@gmail.com>
2021-02-26 01:47:03 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:minesweeper/ui/match_screen.dart';
 import 'package:minesweeper/ui/start_screen.dart';
-
-// We create a "provider", which will store a value (here "Hello world").
-// By using a provider, this allows us to mock/override the value exposed.
 
 void main() {
   runApp(
-    // For widgets to be able to read providers, we need to wrap the entire
-    // application in a "ProviderScope" widget.
-    // This is where the state of our providers will be stored.
     ProviderScope(
       child: MyApp(),
     ),
   );
 }
 
-// Note: MyApp is a HookWidget, from flutter_hooks.
+/// Base class for the entire app
 class MyApp extends HookWidget {
   @override
   // ignore: prefer_expression_function_bodies
@@ -26,7 +19,6 @@ class MyApp extends HookWidget {
     return MaterialApp(
       routes: {
         '/': (context) => StartScreen(),
-        '/match': (context) => MatchScreen(),
       },
       initialRoute: '/',
     );
