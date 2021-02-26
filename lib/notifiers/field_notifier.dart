44415e228a33e37e0d2e3e32e85a878ea10c4d0d
Andreas Hoefer <hoefer74@gmail.com>
2021-02-26 16:54:39 +0100

    step d

--- lib/notifiers/field_notifier.dart
+++ lib/notifiers/field_notifier.dart
@@ -1,25 +1,29 @@
-import 'dart:developer';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:minesweeper/models/block.dart';
 import 'package:minesweeper/models/field.dart';
 
+/// The FieldNotifier is the connection between GUI and state. All state
+/// changes go through FieldNotifier. It extends StateNotifier which handles
+/// all GUI updates. Part of Riverpod.
 class FieldNotifier extends StateNotifier<Field> {
+  /// Standard constructor
   FieldNotifier() : super(Field());
 
-  void create({required numRows, required numCols, required mines}) {
+  /// Creates a new playing field ... just by instantiating a new state
+  /// with a new playing field
+  void create({required int numRows, required int numCols, required int mines}) {
     state = Field.withArguments(numRows: numRows, numCols: numCols, mines: mines);
   }
 
+  /// Handles click events from GUI.
   void handleClick(Block block) {
-    if (state.gameLost) {
-      //return; // TODO: Wieder rein nehmen
-    }
+    if (state.gameLost) return;
     if (state.flagMode) {
       block.flagged = !block.flagged;
       state = state.copyWith();
       return;
     }
+    if (block.flagged) return;
     if (block.open) return;
     block.reveal(state.map);
     if (block.mine) {
@@ -34,6 +38,9 @@ class FieldNotifier extends StateNotifier<Field> {
     state = state.copyWith();
   }
 
+  /// Toggles Flag Mode. Enabled flag mode means that the user does not
+  /// want to open a block, but rather to just plant a flag because
+  /// he suspects a bomb there.
   void toggleFlagMode() {
     state = state.copyWith(flagMode: !state.flagMode);
   }
